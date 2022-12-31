22f68727bb0877acedb9628a43b0cac412e2a9ae
aiglebleu36 <118906080+aiglebleu36@users.noreply.github.com>
2022-12-31 19:56:33 +0100

    Update Maping unicorn exterieur

---
+++ unicorn/__resource.lua
@@ -0,0 +1,7 @@
+description 'YePeezFive - Unicorn Exterior'
+name 'Vanilla Unicorn - Exterior [FiveM/YMAP]'
+author 'YePeezFive - https://discord.gg/Q9VyYV8tSY'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
